60a96180494e902a2a0873cd58d57635f9c76767
1andyn <andy.work.dev@gmail.com>
2020-07-20 00:29:24 -1000

    script for managing collection structures

---
+++ db_manage.py
@@ -0,0 +1,11 @@
+import campus
+import term
+import time
+import dbdriver
+import dns
+
+# test mongo driver
+start_time = time.time()
+connection = dbdriver.Database()
+connection.rebuild_term()
+connection.rebuild_campus()
