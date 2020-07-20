8ead6bdbce9f96bc23bd199dcda3ead963adfed3
1andyn <andy.work.dev@gmail.com>
2020-07-20 00:29:08 -1000

    implemented base campus and term retrieval logic. interfaces with pymongo

--- dbdriver.py
+++ dbdriver.py
@@ -1,5 +1,7 @@
 import pymongo
+import datetime
 import dns
+
 
 class Database:
     def __init__(self):
@@ -12,22 +14,60 @@ class Database:
         self.__mgdbClient = client
         self.__db = client.arg_react_sch
 
-    def getDatabases(self):
-        self.__mgdbClient.list_database_names()
+    # returns database object for arg react sch
+    def get_database(self):
+        return self.__db
 
-    def getCollections(self):
-        self.__mgdbClient.list_collection_names()
+    # prints collections in database
+    def print_collections(self):
+        col_list = self.__db.list_collection_names()
+        for col in col_list:
+            print(col)
 
-    def getmgdbClient(self):
-        return self.__mgdbClient
+    # inserts campus list to campus collection
+    def insert_campuses(self, campus_list):
 
-def insertCampuses():
-    return
+        collection = self.__db["campuses"]
+        doc = []
+        for campus in campus_list:
+            doc.append({"strSchoolCode": campus.get_code(),
+                        "strSchoolDesc": campus.get_desc(),
+                        "dtmCreated": datetime.datetime.utcnow()})
+
+        res = collection.insert_many(doc)
+        print(res)
+
+    def insert_terms(self, campus_list):
+
+        collection = self.__db["semester"]
+        doc = []
+
+        for campus in campus_list:
+            for term in campus.get_terms():
+                doc.append({"strSchoolCode": campus.get_code(),
+                            "strTermCode": term.get_code(),
+                            "strTermDesc": term.get_desc(),
+                            "dtmCreated": datetime.datetime.utcnow()})
+
+        res = collection.insert_many(doc)
+        print(res)
 
 
-def insertTerms():
-    return
+    def insert_subjects(self):
+        return
 
 
-def insertCourses():
-    return
+    def insert_courses(self):
+        return
+
+    def rebuild_campus(self):
+        collection = self.__db["campuses"]
+        collection.drop()
+        rebuilt_col = self.__db["campuses"]
+        print("Campus table rebuilt")
+
+    def rebuild_term(self):
+        collection = self.__db["semester"]
+        collection.drop()
+        rebuilt_col = self.__db["semester"]
+        print("Semester table rebuilt")
