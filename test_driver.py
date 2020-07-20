74191918cca499d76a1fabf7d84e5b77f7c2f012
1andyn <andy.work.dev@gmail.com>
2020-07-20 00:28:25 -1000

    test driver script for running and benchmarking functions

--- test_driver.py
+++ test_driver.py
@@ -1,13 +1,35 @@
 import campus
 import term
 import time
+import dbdriver
+import dns
+
+
+#test campus retrieval
 
 # Time how long it takes to Retrieve Campuses
-#start_time = time.time()
-#campus.retrieve_campuses()
-#("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+campus_list = campus.retrieve_campuses()
+print("[Campus Logic] --- %s seconds ---" % (time.time() - start_time))
+
+# test terms retrieval
 
 start_time = time.time()
 #term.ret_terms_for_campus("HAW")
-term.ret_terms_for_list(campus.retrieve_campuses())
-print("--- %s seconds ---" % (time.time() - start_time))
+term.ret_terms_for_list(campus_list)
+print("[Term Logic]--- %s seconds ---" % (time.time() - start_time))
+
+# test mongo driver
+start_time = time.time()
+connection = dbdriver.Database()
+print("[Connect Logic] --- %s seconds ---" % (time.time() - start_time))
+
+# test insert driver for campus
+start_time = time.time()
+connection.insert_campuses(campus_list)
+print("[Insert Campus Logic] --- %s seconds ---" % (time.time() - start_time))
+
+# test insert driver for terms
+start_time = time.time()
+connection.insert_terms(campus_list)
+print("[Insert Term Logic] --- %s seconds ---" % (time.time() - start_time))
