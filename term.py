33e5f25b2e5110ca97b7c87a5fddab8eda40c5a8
1andyn <andy.work.dev@gmail.com>
2020-07-20 19:35:13 -1000

    modified term/semester logic to multithread. optimal is 8 threads

--- term.py
+++ term.py
@@ -3,14 +3,17 @@
 Term (Semester) object
 
 """
-from lxml import html
+import concurrent.futures
+
 import requests  # importing the requests library
+from lxml import html
 
 
 class Term:
-    def __init__(self, term_code, term_desc):
+    def __init__(self, term_code, term_desc, school):
         self.__t_code = term_code
         self.__t_desc = term_desc
+        self.__school = school
         self.__subs = list()
 
     def get_code(self):
@@ -19,11 +22,18 @@ class Term:
     def get_desc(self):
         return self.__t_desc
 
-    def set_terms(self, sub_list):
+    def get_school(self):
+        return self.__school.get_code()
+
+    def set_subs(self, sub_list):
         self.__subs = sub_list
 
+    def get_subs(self):
+        return self.__subs
 
-def ret_terms_for_campus(school_code):
+def ret_terms_for_campus(school):
+    school_code = school.get_code()
+
     # terms list endpoint
     URL = "https://www.sis.hawaii.edu/uhdad/avail.classes?i="
 
@@ -34,7 +44,8 @@ def ret_terms_for_campus(school_code):
     r = requests.get(url=URL + school_code)  # school_code is passed as a parameter
 
     if r.status_code < 200 or r.status_code > 299:
-        raise ("Unexpected status code: ", r.status_code)
+        print ("Unexpected status code, exiting: " + r.status_code)
+        return
 
     tree = html.fromstring(r.content)
 
@@ -50,13 +61,15 @@ def ret_terms_for_campus(school_code):
         if idx != - 1:
             code = code_string[idx + 3::]  # starts after &t=
             #print(code + " " + termdesc_list[desc_idx])
-            termlist.append(Term(code, termdesc_list[desc_idx]))
+            termlist.append(Term(code, termdesc_list[desc_idx], school))
             desc_idx += 1
 
     return termlist
 
 
 def ret_terms_for_list(school_list):
-    for s in school_list:
-        termlist = ret_terms_for_campus(s.get_code())  # create term list for school code
-        s.set_terms(termlist)
+    # 1 thread = 1.8-2 seconds, 4 threads, .6 seconds
+    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        future_termlist = { executor.submit(ret_terms_for_campus, s) : s for s in school_list }
+        for future in concurrent.futures.as_completed(future_termlist):
+            future_termlist[future].set_terms(future.result())
